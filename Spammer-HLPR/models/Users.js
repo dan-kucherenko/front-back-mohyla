834f9bf324e21f49ee8c4656e1c39ca2457ec0c1
Daniil Kucherenko <daniil.ukraine@gmail.com>
2022-10-29 12:53:19 +0300

    Schema for DB added

---
+++ Spammer-HLPR/models/Users.js
@@ -0,0 +1,11 @@
+"use strict"
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+    first_name: {type: String, required: true},
+    last_name: {type: String, required: true},
+    surname: {type: String, required: true},
+    email: {type: String, required: true, unique: true}
+});
+
+module.exports = mongoose.model('Users', UsersSchema);
