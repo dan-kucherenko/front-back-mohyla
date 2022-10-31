2d1bce1f2decebf503e899f688ab107cf4be8fe4
Daniil Kucherenko <daniil.ukraine@gmail.com>
2022-11-01 00:34:18 +0200

    Mailer upd.

--- Spammer-HLPR/mail-sender/send-email.js
+++ Spammer-HLPR/mail-sender/send-email.js
@@ -1,8 +1,8 @@
 "use strict"
-async function sendEmail() {
-    const nodemailer = require("nodemailer");
-    const Users = require("../models/Users");
+const nodemailer = require("nodemailer");
+const Users = require("../models/Users");
 
+async function sendEmail(subject, message) {
     const USERNAME = process.env.SPAMMER_USERNAME;
     const PASSWORD = process.env.SPAMMER_PASSWORD;
     const transporter = nodemailer.createTransport({
@@ -15,17 +15,21 @@ async function sendEmail() {
         }
     });
 
-    const allUsers = Users.find({}, {email: 1, _id: 0});
-    const usersEmails = [];
+    const allUsers = await Users.find({}, {email: 1});
+    let usersEmails = [];
     for (let i in allUsers)
         usersEmails.push(allUsers[i]['email']);
+
+    // const message_selector = document.getElementById('message-selector');
+    // const selected_message = message_selector.options[message_selector.selectedIndex].text;
+    // console.log(selected_message);
     let result = await transporter.sendMail({
         from: USERNAME,
-        to: "daniil.ukraine@gmail.com",
+        to: usersEmails,
         subject: "Miaoo!!",
         text: "Miaoo!"
-
     });
+    console.log(result);
 }
 
-
+module.exports = {sendEmail};
