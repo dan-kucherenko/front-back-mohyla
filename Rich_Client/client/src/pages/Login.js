ff1ca7edb998a1fd122f8939410e44f91e0178de
dan_kucherenko <daniil.ukraine@gmail.com>
2022-11-22 15:41:32 +0200

    Login page finished

--- Rich_Client/client/src/pages/Login.js
+++ Rich_Client/client/src/pages/Login.js
@@ -1,65 +1,50 @@
-import React from "react";
-import $ from 'jquery'
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 function Login() {
-    const login = () => {
-        const adminChecked = $("#admin").checked;
-        const username = $("#username").value;
-        const password = $("#password").value;
-        if (adminChecked) {
-            if (username === 'root' && password === 'root')
-                alert("Hi!");
-                // windows.location.href = "http://localhost:4567/company/employees";
-            else
-                alert("Wrong username or password");
-        }
-    }
+    const navigate = useNavigate();
+    const [adminChecked, setAdmin] = useState(false);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleChangeUsername = (event) => {
+        setUsername(event.target.value);
+    };
+    const handleChangePassword = (event) => {
+        setPassword(event.target.value);
+    };
+    const handleChangeAdmin = (event) => {
+        setAdmin(current => !current);
+    };
+
+    const login = (props) => {
+        if (username === 'root' && password === 'root' && adminChecked)
+            navigate('/employees');
+        else
+            alert("Wrong username or password");
+    };
 
     return (
-            <section class="vh-100">
-                <div class="container-fluid h-custom">
-                    <div class="row d-flex justify-content-center align-items-center h-100">
-                        <div class="col-md-9 col-lg-6 col-xl-5">
-                            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-                                 class="img-fluid" alt="Sample image"/>
-                        </div>
-                        <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-                            <form>
-                                <div class="form-outline mb-4">
-                                    <input id="username" class="form-control form-control-lg"
-                                           placeholder="Enter a valid username"/>
-                                    <label class="form-label" for="username">Username</label>
-                                </div>
-                                <div class="form-outline mb-3">
-                                    <input type="password" id="password" class="form-control form-control-lg"
-                                           placeholder="Enter password"/>
-                                    <label class="form-label" for="password">Password</label>
-                                </div>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="form-check mb-0">
-                                        <input class="form-check-input me-2" type="checkbox" id="admin" value=""/>
-                                        <label class="form-check-label" for="admin">
-                                            I am administrator
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="text-center text-lg-start mt-4 pt-2">
-                                    <button type="button" class="btn btn-primary btn-lg"
-                                            style="padding-left: 2.5rem; padding-right: 2.5rem;" onClick={login}>Login
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+        <div className="col-md-6">
+            <form>
+                <div className="form-group">
+                    <label className="form-label" htmlFor="username">Username</label>
+                    <input id="username" className="form-control form-control-md" onChange={handleChangeUsername}
+                           placeholder="Enter a valid username"/>
                 </div>
-                <div
-                    class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary"
-                    id="copyright_cont">
-                    <div class="text-white mb-3 mb-md-0" id="copyright">
-                        Copyright © 2022. All rights reserved.
-                    </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" className="form-control" id="password" onChange={handleChangePassword}
+                           placeholder="Password"/>
                 </div>
-            </section>
+                <div className="form-check">
+                    <input type="checkbox" className="form-check-input" onChange={handleChangeAdmin} id="admin"/>
+                    <label className="form-check-label" htmlFor="admin">I am
+                        administrator</label>
+                </div>
+                <button type="button" className="btn btn-primary" onClick={login}>Login</button>
+            </form>
+        </div>
     );
 }
 
