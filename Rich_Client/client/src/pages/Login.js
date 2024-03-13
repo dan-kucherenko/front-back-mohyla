136346f2c67fef5231da4e784c38a198764e84c8
dan-kucherenko <daniil.ukraine@gmail.com>
2024-03-13 19:23:19 +0200

    Edit the login page

--- Rich_Client/client/src/pages/Login.js
+++ Rich_Client/client/src/pages/Login.js
@@ -3,7 +3,7 @@ import {useNavigate} from "react-router-dom";
 
 function Login() {
     const navigate = useNavigate();
-    const [adminChecked, setAdmin] = useState(false);
+    const [adminChecked, setAdminChecked] = useState(false);
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
@@ -13,11 +13,11 @@ function Login() {
     const handleChangePassword = (event) => {
         setPassword(event.target.value);
     };
-    const handleChangeAdmin = (event) => {
-        setAdmin(current => !current);
+    const handleChangeAdmin = () => {
+        setAdminChecked(current => !current);
     };
 
-    const login = (props) => {
+    const login = () => {
         if (username === 'root' && password === 'root' && adminChecked)
             navigate('/company/employees');
         else
@@ -25,26 +25,32 @@ function Login() {
     };
 
     return (
-        <div className="col-md-6">
-            <div className="container" id="login-cont">
-                <form>
-                    <div className="form-group">
-                        <label className="form-label" htmlFor="username">Username</label>
-                        <input id="username" className="form-control form-control-md" onChange={handleChangeUsername}
-                               placeholder="Enter a valid username"/>
+        <div className="container">
+            <div className="row justify-content-center mt-5">
+                <div className="col-md-6">
+                    <div className="container" id="login-cont">
+                        <form>
+                            <div className="form-group">
+                                <label className="form-label" htmlFor="username">Username</label>
+                                <input id="username" className="form-control form-control-md"
+                                       onChange={handleChangeUsername}
+                                       placeholder="Enter a valid username"/>
+                            </div>
+                            <div className="form-group">
+                                <label className="form-label" htmlFor="password">Password</label>
+                                <input type="password" className="form-control" id="password"
+                                       onChange={handleChangePassword}
+                                       placeholder="Password"/>
+                            </div>
+                            <div className="form-check">
+                                <input type="checkbox" className="form-check-input" onChange={handleChangeAdmin}
+                                       id="admin"/>
+                                <label className="form-check-label" htmlFor="admin">I am administrator</label>
+                            </div>
+                            <button type="button" className="btn btn-primary mt-2" onClick={login}>Login</button>
+                        </form>
                     </div>
-                    <div className="form-group">
-                        <label className="form-label" htmlFor="password">Password</label>
-                        <input type="password" className="form-control" id="password" onChange={handleChangePassword}
-                               placeholder="Password"/>
-                    </div>
-                    <div className="form-check">
-                        <input type="checkbox" className="form-check-input" onChange={handleChangeAdmin} id="admin"/>
-                        <label className="form-check-label" htmlFor="admin">I am
-                            administrator</label>
-                    </div>
-                    <button type="button" className="btn btn-primary" onClick={login}>Login</button>
-                </form>
+                </div>
             </div>
         </div>
     );
